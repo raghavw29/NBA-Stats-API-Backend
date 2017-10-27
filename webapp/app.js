3ef2af6cfbefeb270e06126b1b57dcafd7213a80
n8m8 <walls@case.edu>
2017-10-26 22:25:24 -0400

    increase flexibility between players/sides
    type curry or irving into either field and press enter

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,12 @@
 var app = angular.module('nbaApp', []);
 app.controller('myController', function($scope, $http) {
   $scope.data = [];
+  
+  $scope.rsData = [];
+  $scope.lsData = [];
+  
+  $scope.curryData = [4,18,1,93,6,43,2,3];
+  $scope.irvingData = [2,7,52,6,75,1,88,5];
 
   $scope.$watch('inRight', function() {
 
@@ -10,27 +16,37 @@ app.controller('myController', function($scope, $http) {
   });
   
   $scope.leftkeypress = function($event) {
-	  if ($event.keyCode === 13) {
-			
-		      if ($scope.inLeft === 'curry') {
-      $scope.lsData = [4,18,1,93,6,43,2,3];
-      $scope.lsShow = true;
+	if ($event.keyCode === 13) {
+	if ($scope.inLeft === 'curry') {
+			$scope.lsData = $scope.curryData;
+			$scope.lsShow = true;
+			$scope.leftimg="img/curry.jpg";
+	}else if ($scope.inLeft === 'irving' ) {
+			$scope.lsData = $scope.irvingData;
+			$scope.lsShow=true;
+			$scope.leftimg="img/irving.jpg";
     } else {
-      $scope.lsData = [];
-      $scope.lsShow = false;
+			$scope.lsData = [];
+			$scope.lsShow = false;
+			$scope.leftimg="";
     }
 	  }
   }
   
     $scope.rightkeypress = function($event) {
 	  if ($event.keyCode === 13) {
-	  console.log("it do");
-		      if ($scope.inRight === 'irving') {
-      $scope.rsData = [2,7,52,6,75,1,88,5];
-      $scope.rsShow = true;
+	  if ($scope.inRight === 'curry') {
+			$scope.rsData = $scope.curryData;
+			$scope.rsShow = true;
+			$scope.rightimg="img/curry.jpg";
+	  }else if ($scope.inLeft === 'irving' ) {
+			$scope.rsData = $scope.irvingData;
+			$scope.rsShow=true;
+			$scope.rightimg="img/irving.jpg";
     } else {
-      $scope.rsShow = false;
-      $scope.rsData = [];
+			$scope.rsData = [];
+			$scope.rsShow = false;
+			$scope.rightimg="";
     }
 	  }
   }
