79e9b38ff88289f1bf6789f35c46ea5f29f37663
n8m8 <walls@case.edu>
2017-10-26 22:13:25 -0400

    only display info when pressing enter

--- webapp/app.js
+++ webapp/app.js
@@ -3,23 +3,37 @@ app.controller('myController', function($scope, $http) {
   $scope.data = [];
 
   $scope.$watch('inRight', function() {
-    if ($scope.inRight === 'irving') {
-      $scope.rsData = [2,7,52,6,75,1,88,5];
-      $scope.rsShow = true;
-    } else {
-      $scope.rsShow = false;
-      $scope.rsData = [];
-    }
+
   });
   $scope.$watch('inLeft', function() {
-    if ($scope.inLeft === 'curry') {
+
+  });
+  
+  $scope.leftkeypress = function($event) {
+	  if ($event.keyCode === 13) {
+			
+		      if ($scope.inLeft === 'curry') {
       $scope.lsData = [4,18,1,93,6,43,2,3];
       $scope.lsShow = true;
     } else {
       $scope.lsData = [];
       $scope.lsShow = false;
     }
-  });
+	  }
+  }
+  
+    $scope.rightkeypress = function($event) {
+	  if ($event.keyCode === 13) {
+	  console.log("it do");
+		      if ($scope.inRight === 'irving') {
+      $scope.rsData = [2,7,52,6,75,1,88,5];
+      $scope.rsShow = true;
+    } else {
+      $scope.rsShow = false;
+      $scope.rsData = [];
+    }
+	  }
+  }
 
 
 })
